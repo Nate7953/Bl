c3463ab60743c8633466a27036759a0d156efb8a
Nate7953 <natee.120@outlook.com>
2025-04-22 10:51:50 -0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -137,7 +137,7 @@ task.spawn(function()
 			serverHop()
 		elseif currentPlayerCount > 7 then
 			updateStatus("Too Many Players", Color3.fromRGB(255, 150, 80))
-			task.wait(4.3)
+			task.wait(4.1)
 			serverHop()
 		else
 			updateStatus("Safe", Color3.fromRGB(0, 255, 0))
