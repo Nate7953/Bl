8a33198b28db9ed965b70aa5aa9ddd86a91cfa66
Nate7953 <natee.120@outlook.com>
2025-04-24 19:49:12 -0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -132,7 +132,7 @@ end
 -- Main loop
 task.spawn(function()
 	while true do
-		task.wait(1)
+		task.wait(4.6)
 		if not toggle then continue end
 		updatePlayerCount()
 		if #Players:GetPlayers() > 8 then
