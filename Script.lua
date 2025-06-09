54ab3dc3a288bacb20d35f222149b74562739be6
Nate7953 <natee.120@outlook.com>
2025-06-09 09:13:33 -0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,4 +1,4 @@
-local wrapperUrl = "https://raw.githubusercontent.com/YourUsername/YourRepo/main/WrapperScript.lua"
+local wrapperUrl = "https://raw.githubusercontent.com/Nate7953/BlockSpin-Auto-Farm-Roblox/refs/heads/main/Script.lua"
 local originalScriptUrl = "https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Garden/Garden-V1.lua"
 
 queue_on_teleport("loadstring(game:HttpGetAsync('" .. wrapperUrl .. "'))()")
